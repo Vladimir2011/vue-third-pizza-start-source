06b85c332fb81bb31be555759fe23229b00e9c62
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2023-05-12 08:54:34 +0300

    update app, add actions for store, refactored code

--- frontend/src/modules/constructor/DoughSelector.vue
+++ frontend/src/modules/constructor/DoughSelector.vue
@@ -1,58 +1,50 @@
 <template>
   <div class="content__dough">
-
     <div class="sheet">
       <h2 class="title title--small sheet__title">Выберите тесто</h2>
 
-      <div class="sheet__content dough">
-        <label
-          v-for="doughType in items"
-          :key="doughType.id"
-          class="dough__input dough__input--light"
-        >
+      <div class="sheet__content">
+        <label v-for="doughType in items" :key="doughType.id" class="dough__input">
           <input
             type="radio"
-            name="dought"
-            :value="doughType.value"
+            name="dough"
+            :value="doughType.id"
+            :checked="doughType.id === modelValue"
             class="visually-hidden"
-            :checked="doughType.value === modelValue"
-            @input="emit('update:modelValue', doughType.value)"
-          >
+            @input="emit('update:modelValue', doughType.id)"
+          />
           <img :src="getImage(doughType.image)" :alt="doughType.name" />
 
           <b>{{ doughType.name }}</b>
           <span>{{ doughType.description }}</span>
         </label>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script setup>
 defineProps({
   modelValue: {
-    type: String,
-    default: "",
+    type: Number,
+    required: true,
   },
   items: {
     type: Array,
     default: () => [],
   },
-});
+})
 
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(['update:modelValue'])
 
-const getImage = image => {
+const getImage = (image) => {
   return new URL(`../../assets/img/${image}`, import.meta.url).href
 }
-
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/ds-system/ds.scss";
-@import "@/assets/scss/mixins/mixins.scss";
+@import '@/assets/scss/ds-system/ds.scss';
+@import '@/assets/scss/mixins/mixins.scss';
 
 .content__dough {
   width: 527px;
@@ -63,29 +55,40 @@ const getImage = image => {
 
 .dough__input {
   position: relative;
+
   margin-right: 8%;
   margin-bottom: 20px;
   padding-left: 50px;
+
   cursor: pointer;
+
   img {
     @include p_center-v;
+
     width: 36px;
     height: 36px;
+
     transition: 0.3s;
+
     border-radius: 50%;
   }
+
   b {
     @include r-s16-h19;
   }
+
   span {
     @include l-s11-h13;
+
     display: block;
   }
+
   &:hover {
     img {
       box-shadow: $shadow-regular;
     }
   }
+
   input {
     &:checked + img {
       box-shadow: $shadow-large;
--- frontend/src/modules/constructor/SauceSelector.vue
+++ frontend/src/modules/constructor/SauceSelector.vue
@@ -2,18 +2,14 @@
   <div class="ingredients__sauce">
     <p>Основной соус:</p>
 
-    <label
-      v-for="sauceType in items"
-      :key="sauceType.id"
-      class="radio ingredients__input"
-    >
+    <label v-for="sauceType in items" :key="sauceType.id" class="radio ingredients__input">
       <input
         type="radio"
         name="sauce"
         :value="sauceType.value"
-        :checked="sauceType.value === modelValue"
-        @input="emit('update:modelValue', sauceType.value)"
-      >
+        :checked="sauceType.id === modelValue"
+        @input="emit('update:modelValue', sauceType.id)"
+      />
       <span>{{ sauceType.name }}</span>
     </label>
   </div>
@@ -22,33 +18,40 @@
 <script setup>
 defineProps({
   modelValue: {
-    type: String,
-    default: "",
+    type: Number,
+    required: true,
   },
   items: {
     type: Array,
     default: () => [],
   },
-});
+})
 
-const emit = defineEmits(["update:modelValue"]);
-
+const emit = defineEmits(['update:modelValue'])
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/ds-system/ds.scss";
+@import '@/assets/scss/ds-system/ds.scss';
 
 .ingredients__sauce {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
+
   width: 100%;
   margin-bottom: 14px;
+
   p {
     @include r-s16-h19;
+
     margin-top: 0;
     margin-right: 16px;
     margin-bottom: 10px;
   }
+}
+
+.ingredients__input {
+  margin-right: 24px;
+  margin-bottom: 10px;
 }
 </style>
